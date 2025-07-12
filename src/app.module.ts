3338178ccfc91ec229dfe597665b711b1cf0cc06
Tiago Schons <tgschons@gmail.com>
2025-07-12 16:46:15 -0300

    Creating the module and table for users.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { EnvironmentType } from './shared/types/environment.type';
+import typeormConfig from './config/typeorm';
 
 @Module({
   imports: [
@@ -13,33 +13,7 @@ import { EnvironmentType } from './shared/types/environment.type';
       envFilePath: ['.env.local', '.env'],
       ignoreEnvFile: process.env.NODE_ENV === 'production',
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => {
-        const socketPath = configService.get<string>('DATABASE_SOCKET');
-
-        return {
-          type: 'mysql',
-          host: socketPath
-            ? undefined
-            : configService.get<string>('DATABASE_HOST'),
-          port: socketPath
-            ? undefined
-            : configService.get<number>('DATABASE_PORT'),
-          username: configService.get<string>('DATABASE_USERNAME'),
-          password: configService.get<string>('DATABASE_PASSWORD'),
-          database: configService.get<string>('DATABASE_NAME'),
-          entities: [],
-          synchronize:
-            configService.get<EnvironmentType>('ENVIRONMENT') === 'development',
-          extra: socketPath
-            ? {
-                socketPath: socketPath,
-              }
-            : undefined,
-        };
-      },
-    }),
+    TypeOrmModule.forRoot(typeormConfig),
     AuthModule,
     UserModule,
   ],
