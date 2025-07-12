1b25c9ad6e2b98c94e81864ffccbff603f9b6429
Tiago Schons <tgschons@gmail.com>
2025-07-12 12:14:13 -0300

    Add TypeORM, MySQL, and configuration dependencies for database integration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,49 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EnvironmentType } from './shared/types/environment.type';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      envFilePath: ['.env.local', '.env'],
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const socketPath = configService.get<string>('DATABASE_SOCKET');
+
+        return {
+          type: 'mysql',
+          host: socketPath
+            ? undefined
+            : configService.get<string>('DATABASE_HOST'),
+          port: socketPath
+            ? undefined
+            : configService.get<number>('DATABASE_PORT'),
+          username: configService.get<string>('DATABASE_USERNAME'),
+          password: configService.get<string>('DATABASE_PASSWORD'),
+          database: configService.get<string>('DATABASE_NAME'),
+          entities: [],
+          synchronize:
+            configService.get<EnvironmentType>('ENVIRONMENT') === 'development',
+          extra: socketPath
+            ? {
+                socketPath: socketPath,
+              }
+            : undefined,
+        };
+      },
+    }),
+    AuthModule,
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
